b96acb05b6db8e6ace7a8db608985b79e58face1
omkaryadav1999 <117476708+omkaryadav1999@users.noreply.github.com>
2023-03-16 13:48:21 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+// // navigation menu in express
+
+
+// app.get("/", (req, resp) => {
+//     resp.send("hello this is Home page")
+// })
+
+// app.get("/about", (req, resp) => {
+//     resp.send("this is the about page")
+// })
+
+// app.get("/contact", (res, resp) => {
+//     resp.send("this is the contact page")
+// })
+
+
+// app.listen(5000);
+
+
+// send the json data;
+
+// app.get("/data", (req, resp) => {
+//     //express.js behind the scean convert this data into the json ;
+//     resp.send([
+//         { id: 1, name: "omkar", age: 23 },
+//         { id: 2, name: "ashwarya", age: 24 }
+//     ])
+// });
+
+// app.listen(5000)
